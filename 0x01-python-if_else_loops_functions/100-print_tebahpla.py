652d1d3fd9e0e48335a2e8ca7d8ede4bfcc99e9a
Kwadwo Boateng Hasford <kboateng346@gmail.com>
2023-08-11 02:22:47 +0000

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-i = 0
-for ch in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(ch - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(122, 96, -1):
+    if i % 2 == 0:
+        ch = chr(i)
+    else:
+        ch = chr(i-32)
+    print("{}".format(ch), end="")
